33a2dce681e2a376a487cbc50eddd3bf7e41819c
Alex Nguyen <alex.nguyen@alfagomma.com.sg>
2025-03-21 10:29:26 +1300

    Update policies

--- infrastructure/modules/backend/main.tf
+++ infrastructure/modules/backend/main.tf
@@ -95,6 +95,22 @@ resource "aws_api_gateway_rest_api" "api" {
   name = "${var.app_name}-api"
 }
 
+resource "aws_api_gateway_rest_api_policy" "api_policy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "execute-api:Invoke"
+        Resource  = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/*"
+      }
+    ]
+  })
+}
+
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
